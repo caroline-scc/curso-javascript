041b48e40614ea6da0e3ff949d6e033437d9bb3c
caroline-scc <60763787+caroline-scc@users.noreply.github.com>
2020-03-26 18:02:05 -0300

    Update todos.js

--- aplicação/todos.js
+++ aplicação/todos.js
@@ -2,23 +2,49 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
-function renderTodos(){
+function renderTodos() {
+  listElement.innerHTML = '';
 
-    for (todo of todos){
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
+  for (todo of todos) { 
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
 
-        todo.Element.appendChild(todoText);
-        listElement.appendChild(todoElement);
-    }
+    var linkElement = document.createElement('a');
+    linkElement.setAttribute('href', '#'); 
+    var linkText = document.createTextNode('Excluir');
+    linkElement.appendChild(linkText);
 
+    var pos = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'delTodo(' + pos + ')');
 
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+    listElement.appendChild(todoElement);
+  }
 }
 
 renderTodos();
+
+function addTodo() {
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+  saveToStorage();
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo; 
+
+function delTodo(pos) {
+  todos.splice(pos, 1); 
+  saveToStorage();
+  renderTodos();
+}
+
+function saveToStorage() {
+
+  localStorage.setItem('list_todos', JSON.stringify(todos));
+}
