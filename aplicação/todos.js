ffd923905816c0901f1fe0af73fc9e6bbb7b5290
caroline-scc <60763787+caroline-scc@users.noreply.github.com>
2020-03-26 13:17:21 -0300

    Create todos.js

---
+++ aplicação/todos.js
@@ -0,0 +1,24 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+];
+
+function renderTodos(){
+
+    for (todo of todos){
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todo.Element.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+
+
+}
+
+renderTodos();
